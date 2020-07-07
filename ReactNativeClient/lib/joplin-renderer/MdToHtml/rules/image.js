241b023749ceed5da21ce251a19ec9bc857a5606
jiangwei618 <351183359@qq.com>
2020-07-07 13:30:25 +0800

    修改图片显示格式居中

--- ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/image.js
+++ ReactNativeClient/lib/joplin-renderer/MdToHtml/rules/image.js
@@ -10,13 +10,21 @@ function installRule(markdownIt, mdOptions, ruleOptions) {
 
 		const token = tokens[idx];
 		const src = utils.getAttr(token.attrs, 'src');
-		const title = utils.getAttr(token.attrs, 'title');
 
 		if (!Resource.isResourceUrl(src) || ruleOptions.plainResourceRendering) return defaultRender(tokens, idx, options, env, self);
 
 		const r = utils.imageReplacement(ruleOptions.ResourceModel, src, ruleOptions.resources, ruleOptions.resourceBaseUrl);
 		if (typeof r === 'string') return r;
-		if (r) return `<img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: title }))}/>`;
+
+		if (r) {
+			const titleStr = token.content.split('=')[0];
+			if (token.content.split('=')[1]) {
+				const width = token.content.split('=')[1].split('*')[0];
+				const height = token.content.split('=')[1].split('*')[1];
+				return `<p class="graph-img" style="text-align: center;"><img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: titleStr }))} style="width:${width};height:${height}" /></p> <p class="graph-text" style="text-align: center">图：${titleStr}</p>`;
+			}
+			return `<p class="graph-img" style="text-align: center;"><img data-from-md ${htmlUtils.attributesHtml(Object.assign({}, r, { title: titleStr }))}/></p> <p class="graph-text" style="text-align: center">图：${titleStr}</p>`;
+		}
 
 		return defaultRender(tokens, idx, options, env, self);
 	};
